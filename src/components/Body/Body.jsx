cd48600335b62b4542224b53e15022130ca9d35d
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-07-02 15:42:40 +0530

    add toolbar design

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import styles from "./Body.module.css";
+import { ArrowDown } from "react-feather";
 const Body = () => {
+  const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
   return (
     <div className={styles.container}>
       <p className={styles.heading}>Resume Builder</p>
+      <div className={styles.toolbar}>
+        <div className={styles.colors}>
+          {colors.map((item) => (
+            <span key={item} style={{ backgroundColor: item }} className={styles.color}></span>
+          ))}
+        </div>
+        <button className={styles.button}>
+          Download <ArrowDown />
+        </button>
+      </div>
     </div>
   );
 };
