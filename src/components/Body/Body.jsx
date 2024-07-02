67fb7b2ba3bea485822ee5e23de6119837a1cbbb
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-07-02 15:28:04 +0530

    add body component

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Body.module.css";
+const Body = () => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.heading}>Resume Builder</p>
+    </div>
+  );
+};
+
+export default Body;
